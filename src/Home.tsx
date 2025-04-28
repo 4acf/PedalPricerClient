8267e28c4f6e0cda9674fbd5795ddef976110d26
4acf <acfree332211@gmail.com>
2025-04-27 20:18:52 -0700

    add example menubar

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,26 @@
 import { AppSidebar } from "@/components/app-sidebar"
+import {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
 import { Separator } from "@/components/ui/separator"
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
+
 export default function Page() {
   return (
     <SidebarProvider>
@@ -14,6 +30,53 @@ export default function Page() {
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
+            <Menubar>
+              <MenubarMenu>
+                <MenubarTrigger>File</MenubarTrigger>
+                <MenubarContent>
+                  <MenubarItem>
+                    New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarItem>
+                    New Window <MenubarShortcut>⌘N</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarItem disabled>New Incognito Window</MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarSub>
+                    <MenubarSubTrigger>Share</MenubarSubTrigger>
+                    <MenubarSubContent>
+                      <MenubarItem>Email link</MenubarItem>
+                      <MenubarItem>Messages</MenubarItem>
+                      <MenubarItem>Notes</MenubarItem>
+                    </MenubarSubContent>
+                  </MenubarSub>
+                  <MenubarSeparator />
+                  <MenubarItem>
+                    Print... <MenubarShortcut>⌘P</MenubarShortcut>
+                  </MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+              <MenubarMenu>
+                <MenubarTrigger>View</MenubarTrigger>
+                <MenubarContent>
+                  <MenubarCheckboxItem>Always Show Bookmarks Bar</MenubarCheckboxItem>
+                  <MenubarCheckboxItem checked>
+                    Always Show Full URLs
+                  </MenubarCheckboxItem>
+                  <MenubarSeparator />
+                  <MenubarItem inset>
+                    Reload <MenubarShortcut>⌘R</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarItem disabled inset>
+                    Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem inset>Toggle Fullscreen</MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem inset>Hide Sidebar</MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+            </Menubar>
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
