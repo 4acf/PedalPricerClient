6fd234856dfb3bad891abfeb1ab3a1259bac4895
4acf <acfree332211@gmail.com>
2025-05-03 00:29:42 -0700

    example draggable node (laggy)

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,19 +1,47 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { AppMenubar } from "./components/app-menubar"
-import { ReactFlow, Background, BackgroundVariant } from "@xyflow/react"
+import { ReactFlow, Background, BackgroundVariant, useNodesState, applyNodeChanges } from "@xyflow/react"
 import { Separator } from "@/components/ui/separator"
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
-import { Mode, Property, ResolveColor, Slate } from "./utils/colors"
 import { ThemeProvider } from '@/components/theme-provider'
 
 import '@xyflow/react/dist/style.css';
 import { ColorSchemeProvider } from "./components/color-scheme-provider"
+import { ItemNode } from "./components/item-node"
+import { useCallback, useState } from "react"
+import { ItemTypes } from "./api/constants"
+
+const nodeTypes = {
+  itemNode: ItemNode,
+};
+const defaultViewport = { x: 0, y: 0, zoom: 1 };
+
+const inch = 20;
+
+const initialNodes = [
+  {
+    id: '1',
+    type: 'itemNode',
+    draggable: true,
+    selectable: true,
+    data: { itemType: ItemTypes.Pedals, id: "293ff675-5ebf-4e2d-16c5-08dd8226a971"}, 
+    position: { x: 0, y: 0 },
+    style: { width: 2 * inch, height: 2 * inch },
+  },
+];
 
 export default function Home() {
+
+  const [nodes, setNodes] = useState(initialNodes);
+
+  const onNodesChange = useCallback(
+    (changes: any) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes],
+  );
 
   return (
     <ThemeProvider>  
@@ -30,8 +58,15 @@ export default function Home() {
             </header>
             <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
               <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
-                <ReactFlow proOptions={{ hideAttribution: true }} className="rounded-2xl">
-                  <Background variant={BackgroundVariant.Dots} color="#3d4d6c" />
+                <ReactFlow 
+                  nodes={nodes}
+                  nodeTypes={nodeTypes}
+                  onNodesChange={onNodesChange}
+                  proOptions={{ hideAttribution: true }} 
+                  className="rounded-2xl"
+                  defaultViewport={defaultViewport}
+                >
+                  <Background variant={BackgroundVariant.Dots} color="#6b6b6b" />
                 </ReactFlow>
               </div>
             </div>
---
+++ src/components/item-node.tsx
@@ -0,0 +1,14 @@
+import { baseUrl, ItemType } from "@/api/constants";
+
+type ItemNodeProps = {
+    itemType: ItemType,
+    id: string,
+}
+
+export function ItemNode({ data } : { data: ItemNodeProps }) {
+
+    return (
+        <img src={`${baseUrl}/${data.itemType}/${data.id}/image`}/>
+    )
+
+}
