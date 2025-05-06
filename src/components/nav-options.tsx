9b4b2d8240e036c3f7e94d4068260b8f0e513fc7
4acf <acfree332211@gmail.com>
2025-05-05 17:21:07 -0700

    ugly, off center alert dialog

--- src/components/nav-options.tsx
+++ src/components/nav-options.tsx
@@ -10,29 +10,67 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { useReactFlow } from "@xyflow/react"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  AlertDialogPortal,
+} from "@/components/ui/alert-dialog"
+import { useCallback } from "react";
 
 export function NavOptions({}) {
+
+  const { setNodes } = useReactFlow();
+
+  const clearCanvas = useCallback(() => {
+    setNodes([]);
+  }, []);
 
   return (
     <SidebarGroup className="group-data-[collapsible=icon]:hidden">
       <SidebarGroupLabel>Options</SidebarGroupLabel>
       <SidebarMenu>
         <SidebarMenuItem key="Clear Canvas">
-        <SidebarMenuButton className="cursor-pointer transition-colors duration-150 ease-in-out hover:text-[#ff6363]">
-              <Trash2 />
-              <span>Clear Canvas</span>
-          </SidebarMenuButton>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <SidebarMenuButton  
+                className="cursor-pointer transition-colors duration-150 ease-in-out hover:text-[#ff6363]"
+              >
+                <Trash2 />
+                <span>Clear Canvas</span>
+              </SidebarMenuButton>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Clear Canvas?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  Clearing the canvas will also erase pricing details.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={clearCanvas}>Continue</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         </SidebarMenuItem>
         <SidebarMenuItem key="Pricing">
-          <SidebarMenuButton className="cursor-pointer transition-colors duration-150 ease-in-out hover:text-[#00ff94] ">
-              <DollarSign />
-              <span>Pricing</span>
+          <SidebarMenuButton className="cursor-pointer transition-colors duration-150 ease-in-out hover:text-[#00e68a] ">
+            <DollarSign />
+            <span>Pricing</span>
           </SidebarMenuButton>
         </SidebarMenuItem>
         <SidebarMenuItem key="About">
           <SidebarMenuButton className="cursor-pointer transition-colors duration-150 ease-in-out">
-              <Info />
-              <span>About</span>
+            <Info />
+            <span>About</span>
           </SidebarMenuButton>
         </SidebarMenuItem>
       </SidebarMenu>
