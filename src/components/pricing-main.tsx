1ac4c1632b8807e594b9cbcc266578aaac688b92
4acf <acfree332211@gmail.com>
2025-05-14 19:28:04 -0700

    rework pricing table headers, add checkbox (no functionality yet)

--- src/components/pricing-main.tsx
+++ src/components/pricing-main.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { SheetFooter, SheetHeader, SheetTitle } from "./ui/sheet";
 import { Table, TableCaption, TableHeader, TableRow, TableHead, TableBody, TableCell } from "./ui/table";
 import { useReactFlow } from "@xyflow/react";
 import { formalizeItemType, toUSD } from "@/utils/string-formatting";
+import { Checkbox } from "./ui/checkbox";
 
 export function PricingMain() {
 
@@ -21,22 +22,24 @@ export function PricingMain() {
             </SheetHeader>
             <div className="px-5 overflow-auto">
                 <Table>
-                    <TableCaption className="sticky bottom-0 bg-background pt-2">Your pricing information.</TableCaption>
+                    <TableCaption className="sticky bottom-0 bg-background pt-2">{nodes.length === 0 ? "No results." : "Your pricing information."}</TableCaption>
                     <TableHeader className="sticky top-0 bg-background">
                         <TableRow>
-                        <TableHead className="w-[100px]">Type</TableHead>
-                        <TableHead>Name</TableHead>
-                        <TableHead>Price</TableHead>
-                        <TableHead className="text-right">Include</TableHead>
+                            <TableHead>Include</TableHead>
+                            <TableHead>Type</TableHead>
+                            <TableHead>Name</TableHead>
+                            <TableHead className="text-right">Price</TableHead>
                         </TableRow>
                     </TableHeader>
                     <TableBody>
                         {nodes.map((node) => (
                             <TableRow key={node.id}>
-                                <TableCell className="font-medium">{formalizeItemType(node.data.itemType)}</TableCell>
+                                <TableCell>
+                                    <Checkbox />
+                                </TableCell>
+                                <TableCell>{formalizeItemType(node.data.itemType)}</TableCell>
                                 <TableCell className="whitespace-normal break-words">{node.data.item.brand} {node.data.item.name}</TableCell>
-                                <TableCell>{toUSD(node.data.price)}</TableCell>
-                                <TableCell className="text-right">yes</TableCell>
+                                <TableCell className="text-right">{toUSD(node.data.price)}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
