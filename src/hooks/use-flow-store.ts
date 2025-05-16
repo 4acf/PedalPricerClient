f10b447764499ed1df851eea09b8a1283e4f8d95
4acf <acfree332211@gmail.com>
2025-05-15 19:24:05 -0700

    remove nodes array from useFlowStore

--- src/hooks/use-flow-store.ts
+++ src/hooks/use-flow-store.ts
@@ -2,13 +2,11 @@ import { Node } from '@xyflow/react';
 import { create } from 'zustand'
 
 type FlowStore = {
-    nodes: Node[],
     setNodes: (updater: (nodes: Node[]) => Node[]) => void;
     setNodesUpdater: (func: (updater: (nodes: Node[]) => Node[]) => void) => void,
 }
 
 export const useFlowStore = create<FlowStore>((set) => ({
-    nodes: [],
     setNodes: () => {},
     setNodesUpdater: (func) => set(() => ({ setNodes: func })),
 }));
