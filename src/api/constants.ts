5a04dc8ad78ff3f5d4a8344294ac38e69730c2c0
4acf <acfree332211@gmail.com>
2025-05-17 23:35:56 -0700

    move api enums from constants to models

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,11 +1,1 @@
 export const baseUrl: string = "https://localhost:7263";
-
-//used for determining which endpoint to reach
-export type ItemType = "pedals" | "pedalboards" | "powersupplies";
-
-//used because i like passing enum values instead of raw strings
-export enum ItemTypes {
-    Pedals = "pedals",
-    Pedalboards = "pedalboards",
-    PowerSupplies = "powersupplies",
-}
--- src/api/models.ts
+++ src/api/models.ts
@@ -9,3 +9,13 @@ export type Item = ItemPreview & {
     height: number,
     filename: string,
 }
+
+//used for determining which endpoint to reach
+export type ItemType = "pedals" | "pedalboards" | "powersupplies";
+
+//used because i like passing enum values instead of raw strings
+export enum ItemTypes {
+    Pedals = "pedals",
+    Pedalboards = "pedalboards",
+    PowerSupplies = "powersupplies",
+}
