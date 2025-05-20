16303540bb977ebbdd518e3405a13e62f686d1fd
4acf <acfree332211@gmail.com>
2025-05-20 14:46:30 -0700

    favicons and light branding

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import {
   BatteryCharging,
   Columns3,
+  Gem,
   Smartphone,
 } from "lucide-react"
 
@@ -12,6 +13,7 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
+  SidebarMenuButton,
   SidebarRail,
 } from "@/components/ui/sidebar"
 import { ItemTypes } from "@/api/models"
@@ -50,7 +52,15 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavOptions />
       </SidebarContent>
       <SidebarFooter>
-        
+        <SidebarMenuButton size="lg">
+          <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-accent text-sidebar-accent-foreground">
+            <Gem className="size-4" />
+          </div>
+          <div className="flex flex-col gap-0.5 leading-none">
+            <span className="font-semibold">Pedal Pricer</span>
+            <span className="">v2.0.0</span>
+          </div>
+        </SidebarMenuButton>
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
