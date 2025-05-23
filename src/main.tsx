67af3ce5c2527e39169d39017c5b98c85c154a81
4acf <acfree332211@gmail.com>
2025-05-22 22:23:01 -0700

    bandaid fix for the mobile bugs

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Home'
+import { useIsMobile } from './hooks/use-mobile'
+
+function App() {
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '1rem',
+          textAlign: 'center',
+          fontFamily: 'system-ui, sans-serif',
+          fontSize: '1.5rem',
+        }}
+      >
+        🚫 This app is currently only supported on desktop browsers. Please switch to a desktop device.
+      </div>
+    )
+  }
+
+  return <Home />
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <App />
   </StrictMode>,
 )
