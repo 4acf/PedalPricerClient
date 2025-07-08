1084ec4c494ea4bf475dd9873575802c34492193
4acf <acfree332211@gmail.com>
2025-07-07 18:54:15 -0700

    remove unnecessary try catch blocks in api functions

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,54 +3,34 @@ import { Item, ItemPreview, ItemType } from "./models";
 
 export const GetInfo = async (itemType: ItemType) : Promise<ItemPreview[]> => {
 
-    try{
-        const response: Response = await fetch(`${baseUrl}/${itemType}/info`, {
-            method: "GET"
-        });
-        if(!response.ok){
-            throw new Error(`${response.status} error: ${response.statusText}`);
-        }
-        const data = await response.json();
-        return data as ItemPreview[];
+    const response: Response = await fetch(`${baseUrl}/${itemType}/info`, {
+        method: "GET"
+    });
+    if(!response.ok){
+        throw new Error(`${response.status} error: ${response.statusText}`);
     }
-    catch (error){
-        throw error;
-    }
-
+    const data = await response.json();
+    return data as ItemPreview[];
 }
 
 export const GetItem = async (itemType: ItemType, id: string) : Promise<Item> => {
-
-    try{
-        const response: Response = await fetch(`${baseUrl}/${itemType}/${id}`, {
-            method: "GET"
-        });
-        if(!response.ok){
-            throw new Error(`${response.status} error: ${response.statusText}`);
-        }
-        const data = await response.json();
-        return data as Item;
+    const response: Response = await fetch(`${baseUrl}/${itemType}/${id}`, {
+        method: "GET"
+    });
+    if(!response.ok){
+        throw new Error(`${response.status} error: ${response.statusText}`);
     }
-    catch (error){
-        throw error;
-    }
-
+    const data = await response.json();
+    return data as Item;
 }
 
 export const GetImage = async (itemType: ItemType, ID: string) : Promise<Blob> => {
-
-    try{
-        const response: Response = await fetch(`${baseUrl}/${itemType}/${ID}/image`, {
-            method: "GET"
-        });
-        if(!response.ok){
-            throw new Error(`${response.status} error: ${response.statusText}`);
-        }
-        const data: Blob = await response.blob();
-        return data;
+    const response: Response = await fetch(`${baseUrl}/${itemType}/${ID}/image`, {
+        method: "GET"
+    });
+    if(!response.ok){
+        throw new Error(`${response.status} error: ${response.statusText}`);
     }
-    catch (error){
-        throw error;
-    }
-
+    const data: Blob = await response.blob();
+    return data;
 }
